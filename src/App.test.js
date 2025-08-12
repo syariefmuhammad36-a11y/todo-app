38b158b1541e81a69b1c9f9307d39e730cee79fb
Saripmuhh <saripanakbazma@gmail.com>
2025-08-12 10:21:53 +0700

    pesan commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+function App() {
+  return (
+    <div>
+      <a href="https://reactjs.org">Learn React</a>
+    </div>
+  );
+}
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+export default App;
